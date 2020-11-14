4f0f6f8cc6d6c7e372a0a54629a529d83fa990d9
Vadim Sedov <sidovsky.develop@gmail.com>
2020-11-14 16:29:01 +0700

    Add sample app for testing

---
+++ Test/Hyperion-Xamarin.Android.TestApp/App.cs
@@ -0,0 +1,25 @@
+﻿using Android.App;
+using Android.Runtime;
+using System;
+
+namespace Hyperion_Xamarin.Android.TestApp
+{
+    [Application(
+        AllowBackup = false,
+        Icon = "@mipmap/ic_launcher",
+        Label = "@string/app_name",
+        SupportsRtl = false,
+        Theme = "@style/AppTheme")]
+    internal class App : Application
+    {
+        protected App(IntPtr javaReference, JniHandleOwnership transfer) : base(javaReference, transfer)
+        {
+        }
+
+        public override void OnCreate()
+        {
+            base.OnCreate();
+            // Custom Application class for MonoRuntimeProviders registration test
+        }
+    }
+}
---
+++ Test/Hyperion-Xamarin.Android.TestApp/MainActivity.cs
@@ -0,0 +1,19 @@
+﻿using Android.App;
+using Android.OS;
+using AndroidX.AppCompat.App;
+
+namespace Hyperion_Xamarin.Android.TestApp
+{
+    [Activity(
+        Label = "@string/app_name",
+        Theme = "@style/AppTheme",
+        MainLauncher = true)]
+    public class MainActivity : AppCompatActivity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            SetContentView(Resource.Layout.activity_main);
+        }
+    }
+}
